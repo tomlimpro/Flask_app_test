ba016c7629673a2c09410defe25fa6ed8eef67e1
tomlimpro <tomlim.pro@gmail.com>
2023-01-22 22:12:53 +0100

    con flask postgres check !

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
 from flask import Flask, render_template
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session,sessionmaker
+import psycopg2
+
+conn = psycopg2.connect(database="mydb", user="user", password="password", host="db")
+cursor = conn.cursor()
 app = Flask(__name__)
-
-
+# Connexion à la base de donnée
+engine = create_engine("postgresql://user:password@db:5432/mydb")
+db=scoped_session(sessionmaker(bind=engine))
 
 @app.route('/')
-
 @app.route('/home')
 def home_page():
     
     return render_template('home.html')
 
+# Test pour récuperer des items manuellement
 @app.route('/market')
 def market_page():
     items = [
@@ -25,6 +32,11 @@ def market_page():
 def playground_page():
        return render_template('playground.html')
 
+@app.route('/list_user', methods=["GET"])
+def list_user():
+    cursor.execute("SELECT * FROM USERS;")
+    result = cursor.fetchall()
+    return render_template('list_user.html', data=result)
 """
 Permet de démarrer l'application uniquement lorsque le fichier est exécuté directement
 et non lorsque le fichier est importé en tant que module dans un autre script.
@@ -32,6 +44,7 @@ et non lorsque le fichier est importé en tant que module dans un autre script.
 La fonction 'app.run' a des paramètres opérationnels tels que 'host' et 'port' qui permettent de configurer
 l'adresse ip et le numéro sur lesquels l'application écoutera les requêtes.
 """
+
 if __name__ == '__main__':
     app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
